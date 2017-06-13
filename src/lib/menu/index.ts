4a817cb5422f7c171c3d4e56a2072332b45ce177
milocosmopolitan <milocosmopolitan@gmail.com>
2017-06-13 10:59:01 -0400

    dropdown reducer added

---
+++ src/lib/menu/index.ts
@@ -0,0 +1,14 @@
+export { MenuModule } from './menu.module';
+
+/* Service & Reducers */
+export { DropdownService } from './dropdown/dropdown.service';
+export { DropdownReducer } from './dropdown/dropdown.reducer';
+
+
+/* MockTestComponents */
+export {
+  MockMenuComponent,
+  MockMenuHeaderComponent,
+  MockMenuButtonComponent,
+  MockMenuItemComponent
+} from './menu.mock';
