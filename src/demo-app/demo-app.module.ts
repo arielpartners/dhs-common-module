279c829e9bd28bd2832f78b585ca5cae1841d22a
milocosmopolitan <milocosmopolitan@gmail.com>
2017-06-15 17:10:51 -0400

    code-viewer component scaffolded

--- src/demo-app/demo-app.module.ts
+++ src/demo-app/demo-app.module.ts
@@ -9,21 +9,25 @@ import { CapDashModule } from '../lib/module';
 import { NgReduxModule } from '@angular-redux/store';
 import { NgReduxRouterModule } from '@angular-redux/router';
 import { StoreModule } from './store/store.module';
+import { MarkdownModule } from 'angular2-markdown';
 
 import { DemoAppComponent } from './demo-app.component';
 import { HomeComponent } from './components/home/home.component';
 import { DemoMenuComponent } from './components/menu/menu.component';
+import {CodeViewerComponent} from './components/code-viewer/code-viewer.component';
 
 @NgModule({
   declarations: [
     DemoAppComponent,
     HomeComponent,
-    DemoMenuComponent
+    CodeViewerComponent,
+    DemoMenuComponent,
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(DemoAppRoute),
+    MarkdownModule.forRoot(),
     CapDashModule.forRoot(),
     NgReduxModule,
     NgReduxRouterModule,
