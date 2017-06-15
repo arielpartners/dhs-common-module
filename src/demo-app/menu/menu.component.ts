d35dc6fc91725a9935ecba064f9222d4818dc840
Graig Janssen <graig.janssen@gmail.com>
2017-06-15 09:36:08 -0400

    Html template for menu

--- src/demo-app/menu/menu.component.ts
+++ src/demo-app/menu/menu.component.ts
@@ -2,18 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'demo-menu',
-  template: `
-    <cd-menu-button id="myMenu" name="menuName">
-      Menu1
-    </cd-menu-button>
-    <cd-menu triggerBy="myMenu">
-      <cd-menu-header icon="fa fa-desktop" text="Header"></cd-menu-header>
-      <cd-menu-item link="/item1">Item1</cd-menu-item>
-      <cd-menu-item link="/item2">Item2</cd-menu-item>
-      <cd-menu-item link="/item3">Item3</cd-menu-item>
-      <cd-menu-item link="/item4">Item4</cd-menu-item>
-    </cd-menu>
-  `
+  templateUrl: './menu.component.html' 
 })
 
 export class DemoMenuComponent {}
