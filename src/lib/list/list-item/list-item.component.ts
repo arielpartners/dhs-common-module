7070e28ec00ecdd46ebb480a8fe629c17e8935b0
Graig Janssen <graig.janssen@gmail.com>
2017-06-15 17:09:25 -0400

    Remove link functions and tests from ListItemComponent

--- src/lib/list/list-item/list-item.component.ts
+++ src/lib/list/list-item/list-item.component.ts
@@ -13,47 +13,10 @@ import { Router } from '@angular/router';
   selector: 'cd-list-item',
   template: `
     <ng-container>
-      <a class="text-ellipsis text-black">
-        <ng-content></ng-content>
-      </a>
+      <ng-content></ng-content>
     </ng-container>
   `,
   styleUrls: ['./list-item.component.less'],
   encapsulation: ViewEncapsulation.None,
 })
-export class ListItemComponent implements OnInit {
-  private _link: string;
-  @Input()
-  get link () {
-    return this._link;
-  }
-  set link (value: string) {
-    this._link = value;
-  }
-  constructor(
-    public ref: ElementRef,
-    private renderer: Renderer2,
-    public router: Router
-  ) {
-    this.navigateTo = this.navigateTo.bind(this);
-  }
-
-  ngOnInit () {
-    this.setLinkAttribute();
-  }
-
-  navigateTo (e) {
-    e.preventDefault();
-    this.router.navigate([this.link]);
-  }
-
-  setLinkAttribute () {
-    const elem = this.ref.nativeElement.firstElementChild;
-    const httpRe = /(http(s?))\:\/\//gi;
-    if (httpRe.test(this.link)) {
-      elem.href = this.link;
-    } else {
-      elem.onclick = this.navigateTo;
-    }
-  }
-}
+export class ListItemComponent {}
