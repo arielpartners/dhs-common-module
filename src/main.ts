441e6a1d50ea8f1b9c98ca508b4279d94af0c50b
milocosmopolitan <milocosmopolitan@gmail.com>
2017-06-16 16:50:24 -0400

    CapDashModule is not DHSModule

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { CapDashModule } from './lib/module';
+import { DHSModule } from './lib/module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(CapDashModule);
+platformBrowserDynamic().bootstrapModule(DHSModule);
