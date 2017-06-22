2e17a321db04823c4f99ed5269523e9b7af7bc8b
milocosmopolitan <milocosmopolitan@gmail.com>
2017-06-22 11:21:08 -0400

    create examples for list item

--- src/demo-app/components/list/list.component.ts
+++ src/demo-app/components/list/list.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'demo-list',
-  templateUrl: './list.component.html'
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.less'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class DemoListComponent {}
