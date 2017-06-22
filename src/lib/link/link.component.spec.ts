6152427e3a6bec4958c98c8d83d2a5d562a7885b
milocosmopolitan <milocosmopolitan@gmail.com>
2017-06-22 11:18:20 -0400

    enhance link module to use routerLink and routerLinkActive

--- src/lib/link/link.component.spec.ts
+++ src/lib/link/link.component.spec.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, tick, TestBed} from '@angular/core/testing';
 import { LinkComponent } from './link.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { RouterLinkWithHref } from '@angular/router';
+import { By } from '@angular/platform-browser';
+import { WindowRef } from '../utils/window.service';
+import { Location } from '@angular/common';
 
-describe('LinkComponent', () => {
+fdescribe('LinkComponent', () => {
   let component: LinkComponent;
   let fixture: ComponentFixture<LinkComponent>;
-
+  let routeLinkEl, routeLinkInstance, location, winRef;
   @Component({
     template: `<div>test</div>`
   })
@@ -21,6 +25,15 @@ describe('LinkComponent', () => {
       declarations: [
         LinkComponent,
         TestComponent
+      ],
+      providers: [
+        {provide: WindowRef, useValue: {
+          nativeWindow: {
+            location: {
+              href: 'http://example.com'
+            }
+          }
+        }}
       ]
     })
     .compileComponents();
@@ -29,6 +42,10 @@ describe('LinkComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LinkComponent);
     component = fixture.componentInstance;
+    routeLinkEl = fixture.debugElement.query(By.directive(RouterLinkWithHref));
+    routeLinkInstance = routeLinkEl.injector.get(RouterLinkWithHref);
+    winRef = fixture.debugElement.injector.get(WindowRef);
+    // location = fixture.debugElement.injector.get(Location);
     fixture.detectChanges();
   });
 
@@ -36,33 +53,60 @@ describe('LinkComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should set path value with provided string', () => {
-    component.path = 'test-link';
-    expect(component.path).toBe('test-link');
+  it('should use RouterLinkWithHref on a tag', () => {
+    expect(routeLinkEl).not.toBeNull();
+    expect(routeLinkEl.name).toBe('a');
   });
 
-  it('should set href when link is external', () => {
-    component.path = 'http://www.google.com/';
-    component.ngOnInit();
-    fixture.detectChanges();
-    const elem = component.ref.nativeElement.firstElementChild;
-    expect(elem.href).toBe('http://www.google.com/');
+  describe('get path() | set path(value: string)', () => {
+    it('should set path value with provided string', () => {
+      component.path = 'test-link';
+      expect(component.path).toBe('test-link');
+    });
   });
 
-  it('should set onclick to navigateTo when link is internal', () => {
-    component.path = 'test';
-    component.ngOnInit();
-    fixture.detectChanges();
-    const elem = component.ref.nativeElement.firstElementChild;
-    expect(elem.onclick).toBe(component.navigateTo);
-  });
-
-  describe('navigateTo()', () => {
-    it('should navigate to provided link', () => {
-      const spy = spyOn(component.router, 'navigate');
-      component.path = 'test';
-      component.navigateTo({preventDefault: () => { return true; }});
+  describe('ngAfterViewInit()', () => {
+    it('should have called setExternalLink() after view initialize', () => {
+      const spy = spyOn(component, 'setLink');
+      component.ngAfterViewInit();
       expect(spy).toHaveBeenCalled();
     });
   });
+
+  describe('setExternalLink()', () => {
+
+    const condition = 'when provided link includes http or https protocol';
+    let onClickRouteLink;
+
+    beforeEach(() => {
+      component.path = 'http://www.google.com/';
+      onClickRouteLink = routeLinkInstance.onClick;
+      component.ngAfterViewInit();
+      fixture.detectChanges();
+    });
+
+    it(`should overwirte href property ${condition}`, () => {
+      const elemHref = component.ref.nativeElement.firstElementChild.href;
+      expect(elemHref).toBe(component.path);
+    });
+
+    it(`should overwrite onClick() method of RouteLinkWithHref ${condition}`, () => {
+      expect(routeLinkInstance.onClick).not.toBe(onClickRouteLink);
+      expect(routeLinkInstance.onClick).toBe(component.onClickRouteLink);
+    });
+
+  });
+
+  describe('onClickRouteLink', () => {
+    it('sdf', fakeAsync(() => {
+      // const spy = spyOn(location, 'go');
+      console.log(winRef)
+      component.path = 'http://www.google.com/';
+      component.ngAfterViewInit();
+      routeLinkEl.triggerEventHandler('click', null);
+      // tick();
+      fixture.detectChanges();
+      // expect(spy).toHaveBeenCalledWith(component.path);
+    }));
+  })
 });
